10e7a95aee56ff6b05c2ee714193e69a7296d030
Portatil Casa <Portatil Casa@Casa>
2019-02-21 10:30:39 +0100

    Copiado Enum Tramo de ReservasV0 de la practica anterior, pasa test 100%

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/Tramo.java
@@ -0,0 +1,21 @@
+package org.iesalandalus.programacion.reservasaulas.modelo.dominio;
+
+public enum Tramo {
+
+	MANANA("Mañana"),
+	TARDE("Tarde");
+	
+	private String cadenaAMostrar;
+	
+	private Tramo(String cadenaAMostrar) {
+	
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+	
+	public String toString() {
+		
+		return this.cadenaAMostrar;
+		
+	}
+}
+
